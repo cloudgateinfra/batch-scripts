90e81712bca4761b9156515fd214c231a30afe0c
cloudgateinfra <nickhenrichs27@gmail.com>
2022-02-11 18:45:19 -0600

    batch scripts

---
+++ win-launch-multiple-exes.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd "C:\Program Files\thinkorswim\
+start thinkorswim.exe"
+cd "C:\Program Files\tastyworks\
+start tastyworks.exe"
+cd "C:\Program Files\Google\Chrome\Application\
+start chrome.exe"
+cd "C:\Users\user\AppData\Local\Programs\Plexamp\
+start Plexamp.exe"
+
+exit
